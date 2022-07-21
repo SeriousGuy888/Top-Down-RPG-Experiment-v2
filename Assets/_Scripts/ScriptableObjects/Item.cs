a59a078c0eab298db9131db43331b0513d81a2ba
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-07-21 00:13:30 -0400

    added more fields to item scriptable object

--- Assets/_Scripts/ScriptableObjects/Item.cs
+++ Assets/_Scripts/ScriptableObjects/Item.cs
@@ -3,7 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
 public class Item : ScriptableObject {
   new public string name = "New Item";
+  [TextArea] public string description;
   public Sprite icon = null;
+
+  // Instances of the same instance of this scriptable object have the same ID.
+  // Therefore, this is used to compare whether two items are of the same type.
+  public int ID => GetInstanceID();
+
+  public bool isStackable;
+  public int maxStackSize = 1;
+
 
   public virtual void Use() {
     Debug.Log("Using item " + name);
