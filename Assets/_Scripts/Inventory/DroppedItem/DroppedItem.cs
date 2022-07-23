a5ac1914c36d4b216412a624d41246c2bfbd67de
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-07-23 17:08:58 -0400

    dropped items animate on pickup and can have quantities now

--- Assets/_Scripts/Inventory/DroppedItem/DroppedItem.cs
+++ Assets/_Scripts/Inventory/DroppedItem/DroppedItem.cs
@@ -7,22 +7,24 @@ using UnityEngine;
 public class DroppedItem : MonoBehaviour {
   private Player player;
   private BoxCollider2D boxCollider;
+  private SpriteRenderer spriteRenderer;
 
   public Item item;
+  public int quantity = 1;
 
   private void Start() {
+    boxCollider = GetComponent<BoxCollider2D>();
+    spriteRenderer = GetComponent<SpriteRenderer>();
+
     if(Application.isPlaying) {
       player = GameManager.Instance.player;
     }
   }
 
   private void Update() {
-#if UNITY_EDITOR
-    var spriteRenderer = GetComponent<SpriteRenderer>();
     if(spriteRenderer != null && item != null) {
       spriteRenderer.sprite = item.icon;
     }
-#endif
   }
 
   private void OnTriggerEnter2D(Collider2D other) {
@@ -32,9 +34,31 @@ public class DroppedItem : MonoBehaviour {
   }
 
   private void PickUp() {
-    Debug.Log("Picking up " + item.name);
-    int quantityRemaining = Inventory.Instance.inventoryData.Add(item, 1);
-    if (quantityRemaining == 0)
-      Destroy(gameObject); // temporary condition
+    int quantityRemaining = Inventory.Instance.inventoryData.Add(item, quantity);
+    if(quantityRemaining == quantity) // if no items were picked up
+      return;
+    quantity = quantityRemaining;
+
+    bool itemsRemain = quantityRemaining != 0;
+    boxCollider.enabled = itemsRemain; // disable collision only if no items remain
+    StartCoroutine(PlayPickupAnimation(itemsRemain));
+  }
+
+  private IEnumerator PlayPickupAnimation(bool itemsRemain) {
+    Vector2 currentScale = transform.localScale;
+    Vector2 zeroScale = Vector2.zero;
+
+    float elapsed = 0;
+    float animDuration = 0.3f;
+    while(elapsed < animDuration) {
+      elapsed += Time.deltaTime;
+      transform.localScale = Vector2.Lerp(currentScale, zeroScale, elapsed / animDuration);
+      yield return null;
+    }
+
+    if(!itemsRemain)
+      Destroy(gameObject);
+    else
+      transform.localScale = currentScale;
   }
 }
